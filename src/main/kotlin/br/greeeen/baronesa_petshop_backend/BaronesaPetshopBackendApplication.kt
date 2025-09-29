aac90392b2defb6e705fb826167589cd87af4002
Hideaki Fukami <denys.fukami@gmail.com>
2025-09-28 21:53:55 -0300

    backend create

---
+++ src/main/kotlin/br/greeeen/baronesa_petshop_backend/BaronesaPetshopBackendApplication.kt
@@ -0,0 +1,11 @@
+package br.greeeen.baronesa_petshop_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BaronesaPetshopBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<BaronesaPetshopBackendApplication>(*args)
+}
---
+++ src/test/kotlin/br/greeeen/baronesa_petshop_backend/BaronesaPetshopBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.greeeen.baronesa_petshop_backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BaronesaPetshopBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
