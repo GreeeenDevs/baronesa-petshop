cc6fc85f96bf646c2438911be191b94488f5daa7
Hideaki Fukami <denys.fukami@gmail.com>
2025-09-29 12:15:23 -0300

    UsuarioRepository criado

---
+++ src/main/kotlin/br/greeeen/baronesa_petshop_backend/repository/UsuarioRepository.kt
@@ -0,0 +1,15 @@
+package br.greeeen.baronesa_petshop_backend.repository
+
+import br.greeeen.baronesa_petshop_backend.model.Usuario
+import org.springframework.stereotype.Repository
+
+@Repository
+interface UsuarioRepository {
+    fun buscarPorId(id: String): Usuario?
+    
+    fun buscarPorEmail(email: String): Usuario?
+
+    fun salvar(usuario: Usuario): Usuario
+
+    fun deletar(id: String)
+}
